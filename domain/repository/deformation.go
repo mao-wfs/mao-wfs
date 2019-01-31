0b5c634cc63d05e15b33c5f468e54f43ade4ab47
scizorman <tueda1207@gmail.com>
2019-01-31 17:12:11 +0900

    [add] Function to get  the latest deformation

--- domain/repository/deformation.go
+++ domain/repository/deformation.go
@@ -10,4 +10,5 @@ import (
 type DeformationRepository interface {
 	StoreByPositionID(posID int64, deformations *model.Deformation) error
 	FindByPositionID(posID int64, beginTime, endTime time.Time) ([]*model.Deformation, error)
+	FindLatestByPositionID(posID int64) (*model.Deformation, error)
 }
