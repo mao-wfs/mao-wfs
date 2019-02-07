fc61399f38009cd512925861cc81acba71e2aabb
scizorman <tueda1207@gmail.com>
2019-02-07 14:42:07 +0900

    [add] Functions to get a parameter of the domain model

--- domain/model/deformation.go
+++ domain/model/deformation.go
@@ -10,3 +10,12 @@ type Deformation struct {
 	PositionID int64
 	Volume     float32
 }
+
+// GetTime gets the time of the Deformation.
+func (d *Deformation) GetTime() time.Time { return d.Time }
+
+// GetPositionID gets the position ID of the Deformation.
+func (d *Deformation) GetPositionID() int64 { return d.PositionID }
+
+// GetVolume gets the volume of the Deformation.
+func (d *Deformation) GetVolume() float32 { return d.Volume }
