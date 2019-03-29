b3c9fdc52bca87738ef8869be9fd82d5ba62a833
scizorman <tueda1207@gmail.com>
2019-03-29 23:45:50 +0900

    [add] Get each parameter of structures

--- domain/model/position.go
+++ domain/model/position.go
@@ -2,15 +2,27 @@ package model
 
 // Position represents a position on the antenna.
 type Position struct {
-	Coordinates Coordinates
 	ID          PositionID
+	Coordinates *Coordinates
 }
 
 // PositionID is the ID of a position on the antenna.
 type PositionID int
+
+// GetID returns "ID".
+func (p *Position) GetID() PositionID { return p.ID }
+
+// GetCoordinates returns "Coordinates".
+func (p *Position) GetCoordinates() *Coordinates { return p.Coordinates }
 
 // Coordinates represents the coordinates of the antenna.
 type Coordinates struct {
 	Radius float32
 	Angle  float32
 }
+
+// GetRadius returns "Radius".
+func (c *Coordinates) GetRadius() float32 { return c.Radius }
+
+// GetAngle returns "Angle".
+func (c *Coordinates) GetAngle() float32 { return c.Angle }
