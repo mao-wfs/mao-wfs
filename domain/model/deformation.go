2d58cf31103112a949e393093a4fda38d5b1b66f
scizorman <tueda1207@gmail.com>
2019-03-29 23:42:51 +0900

    [change] Docs

--- domain/model/deformation.go
+++ domain/model/deformation.go
@@ -11,11 +11,11 @@ type Deformation struct {
 	Volume     float32
 }
 
-// GetTime gets the time of the Deformation.
+// GetTime returns "Time".
 func (d *Deformation) GetTime() time.Time { return d.Time }
 
-// GetPositionID gets the position ID of the Deformation.
-func (d *Deformation) GetPositionID() int64 { return d.PositionID }
+// GetPositionID returns "PositionID".
+func (d *Deformation) GetPositionID() PositionID { return d.PositionID }
 
-// GetVolume gets the volume of the Deformation.
+// GetVolume returns "Volume".
 func (d *Deformation) GetVolume() float32 { return d.Volume }
