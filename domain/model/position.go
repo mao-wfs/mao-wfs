ceabde58c49b18d57eed4162f5fb2981541962f0
scizorman <tueda1207@gmail.com>
2019-04-16 14:01:59 +0900

    [clean] Refactor

--- domain/model/position.go
+++ domain/model/position.go
@@ -1,27 +1,7 @@
 package model
 
-// Position represents a position on the antenna.
-type Position struct {
-	ID          PositionID
-	Coordinates *Coordinates
-}
-
 // PositionID is an ID of a position on the antenna.
 type PositionID int
-
-// NewPosition returns a new position on the antenna.
-func NewPosition(id PositionID, c *Coordinates) *Position {
-	return &Position{
-		ID:          id,
-		Coordinates: c,
-	}
-}
-
-// GetID returns the position ID.
-func (p *Position) GetID() PositionID { return p.ID }
-
-// GetCoordinates returns the coordinates at the position.
-func (p *Position) GetCoordinates() *Coordinates { return p.Coordinates }
 
 // Coordinates represents the coordinates of the antenna.
 type Coordinates struct {
@@ -42,3 +22,23 @@ func (c *Coordinates) GetRadius() float32 { return c.Radius }
 
 // GetAngle returns the angle at the position.
 func (c *Coordinates) GetAngle() float32 { return c.Angle }
+
+// Position represents a position on the antenna.
+type Position struct {
+	ID          PositionID
+	Coordinates *Coordinates
+}
+
+// NewPosition returns a new position on the antenna.
+func NewPosition(id PositionID, c *Coordinates) *Position {
+	return &Position{
+		ID:          id,
+		Coordinates: c,
+	}
+}
+
+// GetID returns the position ID.
+func (p *Position) GetID() PositionID { return p.ID }
+
+// GetCoordinates returns the coordinates at the position.
+func (p *Position) GetCoordinates() *Coordinates { return p.Coordinates }
